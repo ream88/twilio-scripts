31053497f663622b2cb5bc1d2c13130b998fc43b
Mario Uher <uher.mario@gmail.com>
2023-02-23 19:47:32 +0100

    Implement ListCredentials

--- lib/resources.exs
+++ lib/resources.exs
@@ -1,5 +1,5 @@
 defmodule Resources do
-  @base_uri "https://api.twilio.com"
+  @base_url "https://api.twilio.com"
 
   def fetch_resources(name, uri, opts) do
     fetch_resources([], name, uri, opts)
@@ -10,15 +10,28 @@ defmodule Resources do
   end
 
   def fetch_resources(rest, name, uri, opts) do
-    %{body: %{^name => resources, "next_page_uri" => uri}} = Req.get!(@base_uri <> uri, opts)
-    fetch_resources(rest ++ resources, name, uri, opts)
+    case Req.get!(base_url(opts) <> uri, opts) do
+      %{body: %{^name => resources, "next_page_uri" => uri}} ->
+        fetch_resources(rest ++ resources, name, uri, opts)
+
+      %{body: %{^name => resources, "meta" => %{"next_page_url" => nil}}} ->
+        fetch_resources(rest ++ resources, name, nil, opts)
+
+      %{body: %{^name => resources, "meta" => %{"next_page_url" => url}}} ->
+        %{path: uri} = URI.parse(url)
+        fetch_resources(rest ++ resources, name, uri, opts)
+    end
   end
 
   def fetch_resource(uri, opts) do
-    Req.get!(@base_uri <> uri, opts)
+    Req.get!(base_url(opts) <> uri, opts)
   end
 
   def update_resource(uri, opts) do
-    Req.post!(@base_uri <> uri, opts)
+    Req.post!(base_url(opts) <> uri, opts)
+  end
+
+  defp base_url(opts) do
+    Keyword.get(opts, :base_url, @base_url)
   end
 end
---
+++ list_credentials.exs
@@ -0,0 +1,35 @@
+#!/usr/bin/env elixir
+
+Mix.install([
+  {:req, "~> 0.3.4"},
+  {:flow, "~> 1.2.3"}
+])
+
+Code.require_file("./lib/resources.exs")
+Code.require_file("./lib/utils.exs")
+
+defmodule ListCredentials do
+  import Resources
+  import Utils
+
+  def run() do
+    auth = {get_env("TWILIO_ACCOUNT_SID"), get_env("TWILIO_AUTH_TOKEN")}
+
+    fetch_resources("accounts", "/2010-04-01/Accounts.json", auth: auth)
+    |> Flow.from_enumerable()
+    |> Flow.filter(&(&1["status"] == "active"))
+    |> Flow.map(fn %{"sid" => account_sid, "auth_token" => auth_token} ->
+      fetch_resources(
+        "credentials",
+        "/Credentials",
+        base_url: "https://notify.twilio.com/v1",
+        auth: {account_sid, auth_token}
+      )
+      |> Enum.map(&Map.get(&1, "sid"))
+      |> IO.inspect(label: account_sid)
+    end)
+    |> Flow.run()
+  end
+end
+
+ListCredentials.run()
