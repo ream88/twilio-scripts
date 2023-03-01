36466964c182deabcd124c67769a2e56563cb397
Mario Uher <uher.mario@gmail.com>
2023-03-01 10:24:45 +0100

    Add ListSubaccounts

---
+++ list_subaccounts.exs
@@ -0,0 +1,25 @@
+#!/usr/bin/env elixir
+
+Mix.install([
+  {:req, "~> 0.3.4"}
+])
+
+Code.require_file("./lib/resources.exs")
+Code.require_file("./lib/utils.exs")
+
+defmodule ListSubaccounts do
+  import Resources
+  import Utils
+
+  def run() do
+    auth = {get_env("TWILIO_ACCOUNT_SID"), get_env("TWILIO_AUTH_TOKEN")}
+
+    fetch_resources("accounts", "/2010-04-01/Accounts.json", auth: auth)
+    |> filter_master_account()
+    |> Enum.map(fn %{"sid" => account_sid} = attrs ->
+      IO.inspect(attrs, label: account_sid)
+    end)
+  end
+end
+
+ListSubaccounts.run()
